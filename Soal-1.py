441e615cb6894d311a633c1ad80a7cc27aae5164
Stefan Syauta <91357466+stefsyauta@users.noreply.github.com>
2021-10-01 06:58:35 +0700

    Delete Soal-1.py

--- Soal-1.py
+++
@@ -1,5 +0,0 @@
-#Tugas-1 No. 1
-Nama = input("Nama saya:")
-Umur = int(input ("Umur saya:"))
-Tinggi = float(input("Tinggi saya : "))
-print("Nama saya %s, umur saya %d tahun dan tinggi saya %f cm.s" %(Nama, Umur, Tinggi))
